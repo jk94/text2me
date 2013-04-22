d2d77bb9984dad9e1b7375caf01bff2218de0b7b
DerJan <jankoschke@googlemail.com>
2013-04-22 18:34:00 +0200

    SQL-Injection ausgeschaltet, Operations hinzugefuegt

--- Text2MeServer/src/de/jkdh/text2me_server/main/Operations.java
+++ Text2MeServer/src/de/jkdh/text2me_server/main/Operations.java
@@ -13,20 +13,38 @@ public class Operations {
             stmt = dbc.getTheConnection().prepareStatement("INSERT INTO messenger.user (`Telefon`, `Password`) VALUES (?,?)");
             stmt.setString(1, number);
             stmt.setString(2, password);
-            System.out.println(stmt.toString());
         } catch (SQLException ex) {
         }
-
         return dbc.executeSQLInsert(stmt);
     }
 
-    public ResultSet getUser(DB_Connect dbc, String number) {
-        PreparedStatement stmt=null;
-        try{
-        stmt = dbc.getTheConnection().prepareStatement("SELECT * FROM messenger.user WHERE Telefon = ?");
-        stmt.setString(1, number);
-        }catch (SQLException ex){
+    //+++++++++++++++++++++++++++++++++++++++++++++
+    public ResultSet getUser(DB_Connect dbc, String number, String[] column, String[] columncontainment) {
+        PreparedStatement stmt = null;
+        try {
+            String appending = "";
+            if(column.length == columncontainment.length){
+                for(int i=0;i<column.length;i++){
+                //String srtg = columncontainment[i].replaceAll(";", "");
+                appending = appending + " AND ";
+                appending = appending + column[i] + " = \'" + columncontainment[i] + "\'";
+                }
+            }
             
+            stmt = dbc.getTheConnection().prepareStatement("SELECT * FROM messenger.user WHERE Telefon = ?");
+            stmt.setString(1, number);
+        } catch (SQLException ex) {
+        }
+        return dbc.executeSQLQuery(stmt);
+    }
+    //+++++++++++++++++++++++++++++++++++++++++++++
+
+    public ResultSet getUser(DB_Connect dbc, String number) {
+        PreparedStatement stmt = null;
+        try {
+            stmt = dbc.getTheConnection().prepareStatement("SELECT * FROM messenger.user WHERE Telefon = ?");
+            stmt.setString(1, number);
+        } catch (SQLException ex) {
         }
         return dbc.executeSQLQuery(stmt);
     }
@@ -38,7 +56,36 @@ public class Operations {
             stmt.setString(1, String.valueOf(id));
         } catch (SQLException ex) {
         }
+        return dbc.executeSQLQuery(stmt);
+    }
 
+    public ResultSet getMessageByID(DB_Connect dbc, int id) {
+        PreparedStatement stmt = null;
+        try {
+            stmt = dbc.getTheConnection().prepareStatement("SELECT * FROM messenger.message WHERE M_ID = ?");
+            stmt.setString(1, String.valueOf(id));
+        } catch (SQLException ex) {
+        }
+        return dbc.executeSQLQuery(stmt);
+    }
+
+    public ResultSet getMessageBySenderID(DB_Connect dbc, int senderID) {
+        PreparedStatement stmt = null;
+        try {
+            stmt = dbc.getTheConnection().prepareStatement("SELECT * FROM messenger.message WHERE Sender_ID = ?");
+            stmt.setString(1, String.valueOf(senderID));
+        } catch (SQLException ex) {
+        }
+        return dbc.executeSQLQuery(stmt);
+    }
+
+    public ResultSet getMessageByEmpfaengerID(DB_Connect dbc, int empfaengerID) {
+        PreparedStatement stmt = null;
+        try {
+            stmt = dbc.getTheConnection().prepareStatement("SELECT * FROM messenger.message WHERE Empfaenger_ID = ?");
+            stmt.setString(1, String.valueOf(empfaengerID));
+        } catch (SQLException ex) {
+        }
         return dbc.executeSQLQuery(stmt);
     }
 }
