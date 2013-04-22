5eb40eb0a4a0c7eff9dd9dbd212943566cb1f815
DerJan <jankoschke@googlemail.com>
2013-04-22 17:22:36 +0200

    SQL-Injection ausgeschaltet, Operations hinzugefuegt

--- Text2MeServer/src/de/jkdh/text2me_server/main/Control.java
+++ Text2MeServer/src/de/jkdh/text2me_server/main/Control.java
@@ -1,61 +1,56 @@
 package de.jkdh.text2me_server.main;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import de.jkdh.text2me_server.main.connection.ConnectionServer;
 import de.jkdh.text2me_server.main.connection.DB_Connect;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Control {
 
-	private int port;
-	private ConnectionServer connectionserver;
-	private DB_Connect theDatabaseConnection;
-	private Operations ops;
+    private int port;
+    private ConnectionServer connectionserver;
+    private DB_Connect theDatabaseConnection;
+    private Operations ops;
 
-	public Control(int port) {
-		this.port = port;
-		this.ops = new Operations();
-		// this.setDB_Connection(new DB_Connect("localhost", "messenger",
-		// "root", ""));
+    public Control(int port) {
+        this.port = port;
+        this.ops = new Operations();
+        // this.setDB_Connection(new DB_Connect("localhost", "messenger",
+        // "root", ""));
 
-		startServer();
-/*
-		System.out.println(ops.addUser(theDatabaseConnection, "abc",
-				"+491573478"));
-		ResultSet rs = ops.getUser(theDatabaseConnection, "00491234");
-		try {
-			rs.first();
-			System.out.println(rs.getString("Telefon"));
-		} catch (SQLException ex) {
-			Logger.getLogger(Control.class.getName()).log(Level.SEVERE, null,
-					ex);
-		}*/
-	}
+        startServer();
 
-	public final void startServer() {
-		this.setDB_Connection(new DB_Connect("localhost", "messenger", "root",
-				""));
-		ConnectionServer cs = new ConnectionServer(this.port);
-		cs.start();
-		this.setConnectionServer(cs);
-	}
+        System.out.println(ops.addUser(theDatabaseConnection, "abc",
+                "+491573478"));
+        ResultSet rs = ops.getUser(theDatabaseConnection, "00491234");
+        try {
+            rs.first();
+            System.out.println(rs.getString("Telefon"));
+        } catch (SQLException ex) {
+        }
+    }
 
-	public ConnectionServer getConnectionServer() {
-		return this.connectionserver;
-	}
+    public final void startServer() {
+        this.setDB_Connection(new DB_Connect("localhost", "messenger", "root",
+                ""));
+        ConnectionServer cs = new ConnectionServer(this.port);
+        cs.start();
+        this.setConnectionServer(cs);
+    }
 
-	public DB_Connect getDB_Connection() {
-		return this.theDatabaseConnection;
-	}
+    public ConnectionServer getConnectionServer() {
+        return this.connectionserver;
+    }
 
-	private void setConnectionServer(ConnectionServer conserv) {
-		this.connectionserver = conserv;
-	}
+    public DB_Connect getDB_Connection() {
+        return this.theDatabaseConnection;
+    }
 
-	private void setDB_Connection(DB_Connect dbc) {
-		this.theDatabaseConnection = dbc;
-	}
+    private void setConnectionServer(ConnectionServer conserv) {
+        this.connectionserver = conserv;
+    }
+
+    private void setDB_Connection(DB_Connect dbc) {
+        this.theDatabaseConnection = dbc;
+    }
 }
--- Text2MeServer/src/de/jkdh/text2me_server/main/Operations.java
+++ Text2MeServer/src/de/jkdh/text2me_server/main/Operations.java
@@ -21,9 +21,14 @@ public class Operations {
     }
 
     public ResultSet getUser(DB_Connect dbc, String number) {
-        String sql = "SELECT * FROM `messenger`.`user` WHERE Telefon = '" + number + "';";
-        System.out.println(sql);
-        return dbc.executeSQLQuery(sql);
+        PreparedStatement stmt=null;
+        try{
+        stmt = dbc.getTheConnection().prepareStatement("SELECT * FROM messenger.user WHERE Telefon = ?");
+        stmt.setString(1, number);
+        }catch (SQLException ex){
+            
+        }
+        return dbc.executeSQLQuery(stmt);
     }
 
     public ResultSet getUser(DB_Connect dbc, int id) {
