9661618a16f0567cff2002986c34c201338c4236
DerJan <jankoschke@googlemail.com>
2013-04-22 19:25:47 +0200

    SQL-Injection ausgeschaltet, Operations hinzugefuegt

---
+++ Text2MeServer/src/de/jkdh/text2me_server/main/Konstanten.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.jkdh.text2me_server.main;
+
+/**
+ *
+ * @author Admin
+ */
+public class Konstanten {
+    public final static String STATUS_GESENDET = "gesendet";
+    public final static int STATUS_GESENDET_ID = 1;
+    
+    public final static String STATUS_EMPFANGEN = "empfangen";
+    public final static int STATUS_EMPFANGEN_ID = 2;
+    
+    public final static String STATUS_GELESEN = "gelesen";
+    public final static int STATUS_GELESEN_ID = 3;
+}
