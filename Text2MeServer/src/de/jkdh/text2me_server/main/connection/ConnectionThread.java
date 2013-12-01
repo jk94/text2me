037ea519fdf50540e8b79b315ccb6d7b949833a6
Dani <d.hagg@web.de>
2013-12-01 17:00:56 +0100

    blub2

--- Text2MeServer/src/de/jkdh/text2me_server/main/connection/ConnectionThread.java
+++ Text2MeServer/src/de/jkdh/text2me_server/main/connection/ConnectionThread.java
@@ -1,7 +1,6 @@
 package de.jkdh.text2me_server.main.connection;
 
 import de.jkdh.common.ProtocolPacket;
-import de.jkdh.text2me_server.Listener.ValidListener;
 import de.jkdh.text2me_server.main.main;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -43,7 +42,7 @@ public class ConnectionThread extends Thread {
             String s = "";
             while ((s = reader.readLine()) != null) {
                 
-                new ValidListener(main.getControl().getDB_Connection(), new ProtocolPacket(user, reader));
+                //new ValidListener(main.getControl().getDB_Connection(), new ProtocolPacket(user, reader));
 		System.out.println("new ProtocolPacket");
                 //System.out.println(user.getInetAddress().getHostAddress() + ": " + s);
             }
